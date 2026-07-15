f0649be02cad35e2778dc71e59668020acc2f430
Rafaelly Diniz <138537427+y-Diniz@users.noreply.github.com>
2026-07-15 21:55:29 +0000

    feat: ford-johnson base

--- CPP09/ex02/PMergeMe.cpp
+++ CPP09/ex02/PMergeMe.cpp
@@ -17,13 +17,37 @@ PMergeMe & PMergeMe::operator=( const PMergeMe & other ) {
 
 PMergeMe::~PMergeMe() { }
 
-void fordJohnson( std::vector<int> chain ) {
-	std::vector< std::pair<int, int> > pairs;
+std::vector< int > fordJohnson( const std::vector<int> & chain ) {
+
+	size_t size = chain.size();
 	
-	if (chain.size() <= 2) {
-		
+	if (size == 1)
+		return chain;
+	if (size == 2) {
+		if (chain[0] > chain[1]) {
+			std::vector< int > ordered;
+			ordered.push_back(chain[1]);
+			ordered.push_back(chain[0]);
+			return ordered;
+		}
+		else
+			return chain;
 	}
-	for ( int i = 0; i <= chain.size() - 2; i+=2 )
+
+	std::vector< std::pair<int, int> > pairs;
+	size_t i;
+
+	for ( i = 0; i <= size - 2; i+=2 )
 		pairs.push_back( std::make_pair( chain[i], chain[i+1]) );
-	// tratar straggler
+
+	// comparar pares e separar em winners e losers
+
+	std::vector< int > ordered_winners = fordJohnson( winners );
+
+	// inserir perdedores em ordered_winners
+
+	if ( size % 2 != 0 )
+		// inserir o straggler (chain[i]) no ordered_winners
+
+	return ordered_winners;
 }
